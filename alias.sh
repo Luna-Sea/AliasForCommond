82b42a81fb06905b45b97b478f01d11e48324a22
Luna-Sea <89763838+Luna-Sea@users.noreply.github.com>
2023-06-19 01:09:40 +0800

    Update alias.sh

--- alias.sh
+++ alias.sh
@@ -1,14 +1,14 @@
 mkdir /etc/alias/
-echo " alias     Pupdate='apt update'" >> /etc/alias/alias.sh
-echo " alias     Pupgrade='apt upgrade'>> /etc/alias/alias.sh
-echo " alias     Premove='apt remove'>> /etc/alias/alias.sh
-echo " alias     Pinstall='apt install'>> /etc/alias/alias.sh
-echo " alias     Pinstalled='apt list --installed'>> /etc/alias/alias.sh
-echo " alias     Psearch='apt search'>> /etc/alias/alias.sh
-echo " alias     Pshow='apt show'>> /etc/alias/alias.sh
-echo " alias     Pclean='apt clean'>> /etc/alias/alias.sh
-echo " alias     Pautoclean='apt autoclean'>> /etc/alias/alias.sh
-echo " alias     Systemversion="lsb_release -a">> /etc/alias/alias.sh
+echo " alias     Pupdate='apt update'               " >> /etc/alias/alias.sh
+echo " alias     Pupgrade='apt upgrade'             " >> /etc/alias/alias.sh
+echo " alias     Premove='apt remove'               " >> /etc/alias/alias.sh
+echo " alias     Pinstall='apt install'             " >> /etc/alias/alias.sh
+echo " alias     Pinstalled='apt list --installed'  " >> /etc/alias/alias.sh
+echo " alias     Psearch='apt search'               " >> /etc/alias/alias.sh
+echo " alias     Pshow='apt show'                   " >> /etc/alias/alias.sh
+echo " alias     Pclean='apt clean'                 " >> /etc/alias/alias.sh
+echo " alias     Pautoclean='apt autoclean'         " >> /etc/alias/alias.sh
+echo " alias     Systemversion='lsb_release -a'     " >> /etc/alias/alias.sh
 
 
 
