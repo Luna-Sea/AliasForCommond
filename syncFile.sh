f41c93fdc829689512b3816d17dff9b76a0419c9
Luna-Sea <89763838+Luna-Sea@users.noreply.github.com>
2023-07-06 01:34:42 +0800

    Update syncFile.sh

--- syncFile.sh
+++ syncFile.sh
@@ -1,3 +1,5 @@
+echo "remote pc ip"
+read -r ip
 echo "remote account name"
 read -r account
 echo "ssh password or key file path"
@@ -8,6 +10,6 @@ echo "local file path"
 read -r local
 
 tee -a << EOF >> "/etc/crontab"
-0 8 * * * root rsync -avz -e "ssh -i ${password}" ${account}@A:${remote} ${local}
+0 8 * * * root rsync -avz -e "ssh -i ${password}" ${account}@${ip}:${remote} ${local}
 EOF
 
