177085bf9909bd902e84ce59fcfaebd99bf501c5
Luna-Sea <89763838+Luna-Sea@users.noreply.github.com>
2023-08-18 12:06:25 +0800

    Update sshKeyLogSwitch.sh

--- sshKeyLogSwitch.sh
+++ sshKeyLogSwitch.sh
@@ -3,6 +3,7 @@
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
 ssh_config_file="/etc/ssh/sshd_config"
 sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/' $ssh_config_file
 service ssh restart
