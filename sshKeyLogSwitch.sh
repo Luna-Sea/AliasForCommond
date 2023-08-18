096632d91a7711ba4fcd41053e77dba312ab79d1
Luna-Sea <89763838+Luna-Sea@users.noreply.github.com>
2023-08-18 12:17:25 +0800

    Update sshKeyLogSwitch.sh

--- sshKeyLogSwitch.sh
+++ sshKeyLogSwitch.sh
@@ -8,17 +8,15 @@ cat ~/id_rsa.pub >> ~/.ssh/authorized_keys
 
 ssh_config_file="/etc/ssh/sshd_config"
 sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/' $ssh_config_file
-service ssh restart
-echo "SSH configuration updated. Public key authentication enabled, password authentication disabled."
 
 echo -e "${RED}Do you want to disable password authentication? | yes or no${NC}"
 read -r answer
 if [ "$answer" == "yes" ]; then
     sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/' $ssh_config_file
-    service ssh restart
+    systemctl restart ssh
     echo -e "${RED}SSH configuration updated. Public key authentication enabled, password authentication disabled.${NC}"
 else
-    service ssh restart
+    systemctl restart ssh
     echo -e "${RED}SSH configuration updated. Public key authentication enabled, password authentication enabled.${NC}"
 fi
 rm ~/id_rsa.pub
