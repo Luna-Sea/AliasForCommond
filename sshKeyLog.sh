4994acf7d1e52211c0e91bf90ad5011c9223786b
Luna-Sea <89763838+Luna-Sea@users.noreply.github.com>
2023-08-28 11:05:04 +0800

    Update and rename sshKeyLogSwitch.sh to sshKeyLog.sh

--- sshKeyLog.sh
+++ sshKeyLog.sh
@@ -4,7 +4,7 @@ RED='\033[0;31m'
 NC='\033[0m' # No Color
 
 mkdir -p ~/.ssh
-cat ~/id_rsa.pub >> ~/.ssh/authorized_keys
+cat ./id_rsa.pub >> ~/.ssh/authorized_keys
 
 ssh_config_file="/etc/ssh/sshd_config"
 sed -i 's/#PubkeyAuthentication yes/PubkeyAuthentication yes/' $ssh_config_file
